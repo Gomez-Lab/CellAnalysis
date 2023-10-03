8c6f7b98f2ea7f000920844bd5da5cd05ad78962
mkesapra <89179388+mkesapra@users.noreply.github.com>
2023-10-03 16:19:52 -0700

    Update discriminant_analysis_MacrophageSubtypes.m
    Update code with 5-fold cross-validation

--- discriminant_analysis_MacrophageSubtypes.m
+++ discriminant_analysis_MacrophageSubtypes.m
@@ -1,28 +1,58 @@
 %% This code generates the plots for the discriminant analysis of the macrophage subtypes: Fig S4 
 
-%read from txt and plot
+%%read from txt and plot
 close all
 clear
-M0 = readtable('M0.csv');
-M1 = readtable('M1.csv');
-M2 = readtable('M2.csv');
-M_mixed = readtable('M_mixed.csv');
+M0 = readtable('C0.csv');
+M1 = readtable('C1.csv');
+M2 = readtable('C2.csv');
+M3 = readtable('Cs.csv');
+
+M4 = readtable('Cs12_vs0.csv');
+M5 = readtable('Cs01_vs2.csv');
+rng(143)
 %%
-Mm = [ M_mixed; M1];
+Ms = [ M3; M1];
 
-species = Mm.imageName;
+species = Ms.cluster;
 
-spd = Mm.spd;
-per = Mm.per;
+ecc = Ms.ecc;
+sol = Ms.sol;
+cmp = Ms.cmp;
+
+spd = Ms.spd;
+per = Ms.per;
+
+% Create feature matrix
+X = [spd, per];
+
+% Perform 5-fold cross-validation
+cv = cvpartition(species, 'KFold', 5);
+
+% Initialize arrays to store classification results
+accuracies = zeros(5, 1);
+
+for fold = 1:5
+    trainIdx = training(cv, fold);
+    testIdx = test(cv, fold);
+    
+    % Train the classifier on the training data
+    [C, err, ~, ~, coeff] = classify(X(testIdx, :), X(trainIdx, :), species(trainIdx), 'Quadratic');
+    
+    % Compute accuracy on the test data
+    accuracies(fold) = 1 - err;
+end
+
+% Calculate the mean accuracy over all folds
+meanAccuracy = mean(accuracies);
 
 subplot(2, 2, 1)
 
-h01 = gscatter(spd,per,species,'mg','o^',[],'off');
-set(h01,'LineWidth',2)
+h1 = gscatter(spd,per,species,'mg','v^',[],'off');
+set(h1,'LineWidth',2)
 hold on
 
 
-%%
 [X,Y,Z] = meshgrid(linspace(0,1),linspace(0,1),linspace(0,1));
 X = X(:); Y = Y(:);
 [C,err,P,logp,coeff] = classify([X Y],[spd per],...
@@ -34,33 +64,60 @@ h  = gscatter(X,Y,C,'mg','.',1,'LineWidth',2);
 K = coeff(2,1).const;
 L = coeff(2,1).linear ;
 Q = coeff(2,1).quadratic; 
-
 f = @(x,y) K + L(1)*x + L(2)*y + Q(1,1)*x.*x + (Q(1,2)+Q(2,1))*x.*y + Q(2,2)*y.*y;
-h02 = fimplicit(f,[0 1 0 1]);
-set(h02,'Color','k','LineWidth',2,'DisplayName','Decision Boundary')
-axis([0 1 0 1])                            
-legend('Ms 0+2','M1','Location','best','FontSize', 14)
+h2 = fimplicit(f,[0 1 0 1]);
+set(h2,'Color','k','LineWidth',2,'DisplayName','Decision Boundary')
+axis([0 1 0 1])  
 
-xlabel('Speed (µm/min)','FontSize', 18)
+legend('Cluster 0+2','Cluster 1','Location','best','FontSize', 14)
+
+xlabel('Speed (Âµm/min)','FontSize', 18)
 ylabel('Persistence','FontSize', 18)
-title(['{\bf Classification with accuracy %ge: }' ,num2str(100-err*100)],'FontSize', 20)
+title(['{\bf Classification with mean accuracy of }' ,num2str(meanAccuracy * 100)],'FontSize', 14)
 %%
-M01 = [ M0; M1];
+%%read from txt and plot
 
-species = M01.imageName;
+Ms = [ M0; M1];
 
+species = Ms.cluster;
 
-spd = M01.spd;
-per = M01.per;
+ecc = Ms.ecc;
+sol = Ms.sol;
+cmp = Ms.cmp;
+
+spd = Ms.spd;
+per = Ms.per;
+
+% Create feature matrix
+X = [spd, per];
+
+% Perform 5-fold cross-validation
+cv = cvpartition(species, 'KFold', 5);
+
+% Initialize arrays to store classification results
+accuracies = zeros(5, 1);
+
+for fold = 1:5
+    trainIdx = training(cv, fold);
+    testIdx = test(cv, fold);
+    
+    % Train the classifier on the training data
+    [C, err, ~, ~, coeff] = classify(X(testIdx, :), X(trainIdx, :), species(trainIdx), 'Quadratic');
+    
+    % Compute accuracy on the test data
+    accuracies(fold) = 1 - err;
+end
+
+% Calculate the mean accuracy over all folds
+meanAccuracy = mean(accuracies);
 
 subplot(2, 2, 2)
 
-h11 = gscatter(spd,per,species,'rg','o^',[],'off');
-set(h11,'LineWidth',2)
+h1 = gscatter(spd,per,species,'rg','v^',[],'off');
+set(h1,'LineWidth',2)
 hold on
 
 
-%%
 [X,Y,Z] = meshgrid(linspace(0,1),linspace(0,1),linspace(0,1));
 X = X(:); Y = Y(:);
 [C,err,P,logp,coeff] = classify([X Y],[spd per],...
@@ -72,32 +129,58 @@ h  = gscatter(X,Y,C,'rg','.',1,'LineWidth',2);
 K = coeff(2,1).const;
 L = coeff(2,1).linear ;
 Q = coeff(2,1).quadratic; 
-
 f = @(x,y) K + L(1)*x + L(2)*y + Q(1,1)*x.*x + (Q(1,2)+Q(2,1))*x.*y + Q(2,2)*y.*y;
-h12 = fimplicit(f,[0 1 0 1]);
-set(h12,'Color','k','LineWidth',2,'DisplayName','Decision Boundary')
-axis([0 1 0 1])                            
-legend('M0','M1','Location','best','FontSize', 14)
+h2 = fimplicit(f,[0 1 0 1]);
+set(h2,'Color','k','LineWidth',2,'DisplayName','Decision Boundary')
+axis([0 1 0 1])  
 
-xlabel('Speed (µm/min)','FontSize', 18)
+legend('Cluster 0','Cluster 1','Location','best','FontSize', 14)
+
+xlabel('Speed (Âµm/min)','FontSize', 18)
 ylabel('Persistence','FontSize', 18)
-title(['{\bf Classification with accuracy %ge: }' ,num2str(100-err*100)],'FontSize', 20)
+title(['{\bf Classification with mean accuracy of }' ,num2str(meanAccuracy * 100)],'FontSize', 14)
 %%
-M02 = [ M0; M2];
+Ms = [ M0; M2];
 
-species = M02.imageName;
+species = Ms.cluster;
 
-spd = M02.spd;
-per = M02.per;
+ecc = Ms.ecc;
+sol = Ms.sol;
+cmp = Ms.cmp;
+
+spd = Ms.spd;
+per = Ms.per;
+
+% Create feature matrix
+X = [spd, per];
+
+% Perform 5-fold cross-validation
+cv = cvpartition(species, 'KFold', 5);
+
+% Initialize arrays to store classification results
+accuracies = zeros(5, 1);
+
+for fold = 1:5
+    trainIdx = training(cv, fold);
+    testIdx = test(cv, fold);
+    
+    % Train the classifier on the training data
+    [C, err, ~, ~, coeff] = classify(X(testIdx, :), X(trainIdx, :), species(trainIdx), 'Quadratic');
+    
+    % Compute accuracy on the test data
+    accuracies(fold) = 1 - err;
+end
+
+% Calculate the mean accuracy over all folds
+meanAccuracy = mean(accuracies);
 
 subplot(2, 2, 3)
 
-h1 = gscatter(spd,per,species,'rb','o^',[],'off');
+h1 = gscatter(spd,per,species,'rb','v^',[],'off');
 set(h1,'LineWidth',2)
 hold on
 
 
-%%
 [X,Y,Z] = meshgrid(linspace(0,1),linspace(0,1),linspace(0,1));
 X = X(:); Y = Y(:);
 [C,err,P,logp,coeff] = classify([X Y],[spd per],...
@@ -109,20 +192,21 @@ h  = gscatter(X,Y,C,'rb','.',1,'LineWidth',2);
 K = coeff(2,1).const;
 L = coeff(2,1).linear ;
 Q = coeff(2,1).quadratic; 
-
 f = @(x,y) K + L(1)*x + L(2)*y + Q(1,1)*x.*x + (Q(1,2)+Q(2,1))*x.*y + Q(2,2)*y.*y;
 h2 = fimplicit(f,[0 1 0 1]);
 set(h2,'Color','k','LineWidth',2,'DisplayName','Decision Boundary')
-axis([0 1 0 1])                            
-legend('M0','M2','Location','best','FontSize', 14)
+axis([0 1 0 1])  
 
-xlabel('Speed (µm/min)','FontSize', 18)
+legend('Cluster 0','Cluster 2','Location','best','FontSize', 14)
+
+xlabel('Speed (Âµm/min)','FontSize', 18)
 ylabel('Persistence','FontSize', 18)
-title(['{\bf Classification with accuracy %ge: }' ,num2str(100-err*100)],'FontSize', 20)
+title(['{\bf Classification with mean accuracy of }' ,num2str(meanAccuracy * 100)],'FontSize', 14)
+%%
 %%
 Ms = [ M1; M2];
 
-species = Ms.imageName;
+species = Ms.cluster;
 
 ecc = Ms.ecc;
 sol = Ms.sol;
@@ -130,34 +214,61 @@ cmp = Ms.cmp;
 
 spd = Ms.spd;
 per = Ms.per;
-cellSize = Ms.cellSize;
+
+% Create feature matrix
+X = [spd, per];
+
+% Perform 5-fold cross-validation
+cv = cvpartition(species, 'KFold', 5);
+
+% Initialize arrays to store classification results
+accuracies = zeros(5, 1);
+
+for fold = 1:5
+    trainIdx = training(cv, fold);
+    testIdx = test(cv, fold);
+    
+    % Train the classifier on the training data
+    [C, err, ~, ~, coeff] = classify(X(testIdx, :), X(trainIdx, :), species(trainIdx), 'Quadratic');
+    
+    % Compute accuracy on the test data
+    accuracies(fold) = 1 - err;
+end
+
+% Calculate the mean accuracy over all folds
+meanAccuracy = mean(accuracies);
 
 subplot(2, 2, 4)
 
-h31 = gscatter(spd,per,species,'gb','o^',[],'off');
-set(h31,'LineWidth',2)
+h1 = gscatter(spd,per,species,'gb','v^',[],'off');
+set(h1,'LineWidth',2)
 hold on
 
 
-%%
 [X,Y,Z] = meshgrid(linspace(0,1),linspace(0,1),linspace(0,1));
 X = X(:); Y = Y(:);
 [C,err,P,logp,coeff] = classify([X Y],[spd per],...
                                 species,'Quadratic');
  
 hold on;
-h  = gscatter(X,Y,C,'bg','.',1,'LineWidth',2);
+h  = gscatter(X,Y,C,'gb','.',1,'LineWidth',2);
 
 K = coeff(2,1).const;
 L = coeff(2,1).linear ;
 Q = coeff(2,1).quadratic; 
-
 f = @(x,y) K + L(1)*x + L(2)*y + Q(1,1)*x.*x + (Q(1,2)+Q(2,1))*x.*y + Q(2,2)*y.*y;
-h32 = fimplicit(f,[0 1 0 1]);
-set(h32,'Color','k','LineWidth',2,'DisplayName','Decision Boundary')
+h2 = fimplicit(f,[0 1 0 1]);
+set(h2,'Color','k','LineWidth',2,'DisplayName','Decision Boundary')
+axis([0 1 0 1])  
+
+legend('Cluster 1','Cluster 2','Location','best','FontSize', 14)
+
+xlabel('Speed (Âµm/min)','FontSize', 18)
+ylabel('Persistence','FontSize', 18)
+title(['{\bf Classification with mean accuracy of }' ,num2str(meanAccuracy * 100)],'FontSize', 14)
 axis([0 1 0 1])                            
 legend('M1','M2','Location','best','FontSize', 14)
 
-xlabel('Speed (µm/min)','FontSize', 18)
+xlabel('Speed (Âµm/min)','FontSize', 18)
 ylabel('Persistence','FontSize', 18)
 title(['{\bf Classification with accuracy %ge: }' ,num2str(100-err*100)],'FontSize', 20)
